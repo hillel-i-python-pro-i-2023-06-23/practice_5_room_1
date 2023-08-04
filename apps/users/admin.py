1c0a26c1e791962d14563c211bfda6c61cb4cc5a
YevhenYalovenko <jenyaya0007@gmail.com>
2023-08-04 19:04:42 +0300

    Add admin for project

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+from .models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email", "password", "created_at", "modified_at")
+    list_filter = ("created_at", "modified_at")
+    search_fields = ("name", "email")
+
+
+admin.site.register(User, UserAdmin)
