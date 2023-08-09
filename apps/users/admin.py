68d474dad3e8766a86b4a0a9b92265bf53aade96
YevhenYalovenko <jenyaya0007@gmail.com>
2023-08-09 19:08:24 +0300

    Finish base project

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
+
 from .models import User
 
 
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "email", "password", "created_at", "modified_at")
     list_filter = ("created_at", "modified_at")
     search_fields = ("name", "email")
-
-
-admin.site.register(User, UserAdmin)
